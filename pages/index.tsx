1369181ee4265f26231281cd27d9cb8216d37f45
Biruk Endris <biruketblem@gmail.com>
2022-07-10 03:01:33 +0200

    add new page and new design style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import Nav from "../components/nav";
-import styles from "../styles/Home.module.css";
-import { motion } from "framer-motion";
 import Profile from "../components/profile";
 
 const Home: NextPage = () => {
