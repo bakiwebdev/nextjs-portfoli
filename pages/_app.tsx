f607655d32151706cd3bc5556e3e9abecacda406
Biruk Endris <57604289+Biruk-hub@users.noreply.github.com>
2022-12-05 22:48:22 +0100

    overflow-x-hidden

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Roboto } from '@next/font/google'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Roboto } from "@next/font/google";
 
 const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-})
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={roboto.className}>
-      <div className='relative w-screen h-screen overflow-scroll'>
-        <div className='relative max-w-7xl w-full  mx-auto px-4 md:px-10'>
+      <div className="relative w-screen h-screen overflow-y-scroll overflow-x-hidden">
+        <div className="relative max-w-7xl w-full  mx-auto px-4 md:px-10">
           <Component {...pageProps} />
         </div>
       </div>
     </main>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
