d62c28d1ccf85fbb1fb401b188adf241455fa292
Biruk Endris <57604289+Biruk-hub@users.noreply.github.com>
2022-12-06 23:44:33 +0100

    Update CustomerChallenge.tsx

--- components/CustomerChallenge.tsx
+++ components/CustomerChallenge.tsx
@@ -2,8 +2,7 @@ const CustomerChallenge = () => {
   return (
     <section className="relative w-full h-fit px-2 md:px-16 py-16 md:py-20 flex flex-col justify-between gap-4">
       <h1 className="text-2xl md:text-5xl font-semibold space-y-5 tracking-wide">
-        Let me know what your biggest challenge is and I&apos;ll work with you
-        to create a solution!
+        Why Is Your Business Struggling to Attract Customers?
       </h1>
     </section>
   );
