86107d97535b863c2a97f437cf52565322cd4a2b
Biruk Endris <57604289+Biruk-hub@users.noreply.github.com>
2022-12-14 13:17:59 +0100

    insert Say Hello page on the hero section

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -42,6 +42,10 @@ const Profile = () => {
         technical know-how to make products that are both visually delightful
         and highly efficient.
       </p>
+      <button className="w-fit px-10 py-3 bg-gray-800 text-white cursor-pointer rounded-md flex justify-center items-center gap-5 active:bg-gray-700">
+        <span>Say Hello</span>
+        <span className="text-2xl">👋</span>
+      </button>
       {/* Social Links */}
       <div className="py-6 flex flex-row justify-start items-center gap-8 lg:gap-14 fill-gray-400">
         <IconButton icon={<TwitterIcon />} onClick={handleTwitterClick} />
