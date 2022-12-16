b492bc51602f28835f0cceb03f50ada0aef5d64f
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2022-12-16 01:01:25 +0100

    create about page

---
+++ pages/about.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Footer from "../components/Footer";
+import Nav from "../components/nav";
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>
+          Welcome, I&apos;m Biruk Endris a UI/UX Designer and Front-End Dev
+        </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        {/* add image */}
+        <meta name="title" content="baki.ruqad.com" />
+        <meta
+          name="description"
+          content="A UI/UX Designer and Web Developer. I specialize in Next JS, Tailwind CSS, and creating         user-friendly, efficient, and visually appealing web apps."
+        />
+        <meta
+          name="keywords"
+          content="freelancer, web service, web design, UI/UX, portfolio, website, developer, artist, full stack, next.js,        tailwind, react, framer motion, typescript, JavaScript, HTML, CSS, Node JS, express JS, AWS, telegram bot, bot"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Biruk Endris" />
+      </Head>
+      <Nav />
+      <div className="flex h-[25rem] w-full bg-gray-500">hey</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
